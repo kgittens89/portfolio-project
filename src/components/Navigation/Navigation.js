833eb18a7eb5b2e6078e6e51f7ec1ddfcc2a5850
Keisha Gittens <keishamsa@me.com>
2022-02-05 17:22:58 -0600

    Add favicon & Nav component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function Navigation(props) {
+    return (
+        <div>
+            Navigation 
+        </div>
+    );
+}
+
+export default Navigation;
