a482d8ab0192920573f814baa25be3e4c2f66753
Keisha Gittens <keishamsa@me.com>
2022-02-06 18:11:18 -0600

    Add footer component with social links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { BsGithub } from 'react-icons/bs';
+import { BsLinkedin } from 'react-icons/bs';
+import { AiFillTwitterCircle } from 'react-icons/ai';
+import { MdEmail } from 'react-icons/md'
+import './Footer.css'
 
-function Footer(props) {
+function Footer() {
     return (
-        <div>
-            Footer
-        </div>
-    );
-}
-
-export default Footer;
+			<footer>
+				<p>Keisha M. Gittens, 2022</p>
+				<div>
+					<a
+						href='https://github.com/kgittens89'
+						target='_blank'
+						rel='noreferrer'>
+						<BsGithub className='icons' />
+					</a>
+					<a
+						href='https://www.linkedin.com/in/keisha-gittens/'
+						target='_blank'
+						rel='noreferrer'>
+						<BsLinkedin className='icons' />
+					</a>
+					<a
+						href='https://twitter.com/?lang=en'
+						target='_blank'
+						rel='noreferrer'>
+						<AiFillTwitterCircle className='icons' />
+					</a>
+					<MdEmail className='icons' />
+				</div>
+			</footer>
+		);
+    }
+    
+    export default Footer;
