95073337b0dd490dab873e6126e089d2c4fbf551
Keisha Gittens <keishamsa@me.com>
2022-02-09 18:48:27 -0600

    Add hyperlink for resume, Add animate framework for home screen animation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import 'animate.css';
 
 function Home(props) {
     return (
-        <div className='home'>
-            <h1>Keisha M. Gittens</h1>
-            <h4>Software Engineer</h4>
-        </div>
-    );
+			<div className='home animate__animated animate__rubberBand animate__repeat-2'>
+				<h1>Keisha M. Gittens</h1>
+				<h4>Software Engineer</h4>
+			</div>
+		);
 }
 
 export default Home;
