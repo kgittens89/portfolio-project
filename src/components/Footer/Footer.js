e420d29c531661168567a71efef485b6417f54be
Keisha Gittens <keishamsa@me.com>
2022-02-12 14:35:00 -0600

    Update footer margin, Add tooltip to email icon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { BsGithub } from 'react-icons/bs';
 import { BsLinkedin } from 'react-icons/bs';
 import { AiFillTwitterCircle } from 'react-icons/ai';
-import { MdEmail } from 'react-icons/md'
+import { MdEmail } from 'react-icons/md';
+import { Tooltip } from '@mui/material';
 import './Footer.css'
 
 function Footer() {
@@ -28,12 +29,11 @@ function Footer() {
 						rel='noreferrer'>
 						<AiFillTwitterCircle className='icons' />
 					</a>
-					<a
-						href='mailto:keishamsa@me.com'
-						target='_blank'
-						rel='noreferrer'>
-						<MdEmail className='icons' />
+						<Tooltip title='Email Me'>
+					<a href='mailto:keishamsa@me.com' target='_blank' rel='noreferrer'>
+							<MdEmail className='icons' />
 					</a>
+						</Tooltip>
 				</div>
 			</footer>
 		);
