7c9e717ec3eb9823dff1c53e6105eb28d878dd12
Keisha Gittens <keishamsa@me.com>
2022-02-13 11:02:58 -0600

    Add react-animated text to use on home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import 'animate.css';
+import { Random } from 'react-animated-text';
 
 function Home(props) {
     return (
-		<div className='home
-			 component
-			 animate__animated animate__rubberBand'>
-				<h1>Keisha M. Gittens</h1>
-				<h4>Software Engineer</h4>
+			<div className='home component'>
+				<h1 className='animate__animated animate__rubberBand'>
+					Keisha M. Gittens
+				</h1>
+				<Random
+					className='head'
+					text='Software Engineer'
+					iterations='1'
+					effect='verticalFadeIn'
+					effectChange={2}
+					effectDirection='down'
+				/>
+				{/* <h4>Software Engineer</h4> */}
 			</div>
 		);
 }
