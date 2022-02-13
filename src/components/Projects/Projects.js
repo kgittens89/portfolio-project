996c7c6b7b2bb4c6edeaa6907285bb4448724980
Keisha Gittens <keishamsa@me.com>
2022-02-13 08:49:05 -0600

    Add project descriptions

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -34,8 +34,10 @@ function Projects() {
 									Spaceman Game
 								</Typography>
 								<Typography variant='body2'>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Necessitatibus, optio. Eos quod reiciendis autem corporis?
+									Spaceman is a browser-based two-player game for word-guessing
+									fanatics! Users are able to input a word to be guessed by
+									their opponent. The game is won when either the word is
+									guessed or you run out of chances. Happy guessing!
 								</Typography>
 							</CardContent>
 							<CardActions>
@@ -75,8 +77,11 @@ function Projects() {
 									Let's Cook
 								</Typography>
 								<Typography variant='body2'>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Necessitatibus, optio. Eos quod reiciendis autem corporis?
+									Let's Cook is an application built to accomodate indecisive
+									eaters. You are initially presented with random recipes to
+									choose from. Users can also search for something that may
+									interest them, or generate another random recipe. Happy
+									cooking!
 								</Typography>
 							</CardContent>
 							<CardActions>
@@ -115,6 +120,55 @@ function Projects() {
 									The Flow Factory
 								</Typography>
 								<Typography variant='body2'>
+									The Flow Factory is a yoga application designed for both, new
+									yogis & teachers. Users are able to browse a library of yoga
+									poses and create their own sequences, saving them to the
+									database. Enjoy the flow!
+								</Typography>
+							</CardContent>
+							<CardActions>
+								<Button
+									size='small'
+									href='https://clever-montalcini-9e14ea.netlify.app/'
+									target='_blank'
+									rel='noreferrer'>
+									Deployment
+								</Button>
+								<Button
+									size='small'
+									href='https://github.com/kgittens89/yoga-frontend'
+									target='_blank'
+									rel='noreferrer'>
+									Frontend Repo
+								</Button>
+								<Button
+									size='small'
+									href='https://github.com/kgittens89/yoga'
+									target='_blank'
+									rel='noreferrer'>
+									Backend Repo
+								</Button>
+							</CardActions>
+						</CardActionArea>
+					</Card>
+					{/* <Card
+						sx={{
+							maxWidth: 345,
+							padding: '1em',
+							margin: '1em',
+						}}>
+						<CardActionArea>
+							<CardMedia
+								component='img'
+								height='200'
+								image={flow}
+								alt='The Flow Factory'
+							/>
+							<CardContent>
+								<Typography gutterBottom variant='h5' component='div'>
+									The Flow Factory
+								</Typography>
+								<Typography variant='body2'>
 									Lorem ipsum dolor sit amet consectetur adipisicing elit.
 									Necessitatibus, optio. Eos quod reiciendis autem corporis?
 								</Typography>
@@ -136,7 +190,7 @@ function Projects() {
 								</Button>
 							</CardActions>
 						</CardActionArea>
-					</Card>
+					</Card> */}
 				</div>
 			</div>
 		</>
