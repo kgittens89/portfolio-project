07852c3de05d40bd36d53a7c650b8b643b66f2df
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2022-02-17 23:10:10 -0600

    Update Projects display & About me section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,44 +3,21 @@ import Image from 'react-bootstrap/Image';
 import './AboutMe.css';
 import womanCoding from '../../assets/modern-young-woman-sitting-on-260nw-1928965898.jpeg';
 
-function AboutMe(props) {
+function AboutMe() {
     return (
 			<div className='component about'>
 				<h2>About Me</h2>
 				<h4>Software Engineer</h4>
 				<Image src={womanCoding} circle />
 				<p>
-					Hi there 👋🏾, I'm Keisha, a Software Engineer based out of New Orleans,
-					LA. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-					Expedita, saepe qui fugit quos est explicabo facilis recusandae
-					obcaecati illo eum. Sit, maxime laudantium explicabo, soluta placeat
-					fuga dolorum cupiditate veniam laboriosam, itaque esse numquam. Est
-					hic aliquam nisi perspiciatis officiis error nulla autem, maiores
-					omnis magnam, alias dolore laudantium. Vel.
+				Hi there 👋🏾, I'm Keisha, a Software Engineer, with a background in healthcare,  based out of New Orleans, LA.
+				I've recently completed an intense Software Engineering Immersive with General Assembly, and am ready to put my new skills to work! 
+				I'm a critical thinker with a passion for developing creative, easy to understand, solutions to complex problems. When I'm not coding, I enjoy travel, yoga, and curling up with a captivating book. Want to hear more, reach out! I'd love to connect.
 				</p>
 				<h4>Skills</h4>
 				<p>
 					Javascript | React | Python | HTML | CSS | Bootstrap | MongoDB |
 					Mongoose | Express | Git | Github | CLI | Django | PostgreSQL
-				</p>
-				{/* <ul>
-                <li>Javascript</li>
-                <li>React</li>
-                <li>HTML/CSS</li>
-                <li>Python</li>
-                <li>MongoDB/Mongoose</li>
-                <li>Express</li>
-                <li>Git/Github</li>
-                <li>CLI</li>
-                <li>Bootstrap</li>
-            </ul> */}
-				<h4>Hobbies</h4>
-				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
-					voluptate doloremque sequi autem nostrum repellat temporibus placeat
-					blanditiis quidem exercitationem, hic mollitia illum reprehenderit,
-					praesentium accusantium repellendus quis repudiandae? Eligendi a quod
-					nihil praesentium.
 				</p>
 			</div>
 		);
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,78 +26,43 @@ function Projects() {
 							<CardMedia
 								component='img'
 								height='200'
-								image={spaceman}
-								alt='Spaceman Game'
+								image='https://www.pymnts.com/wp-content/uploads/2016/06/Slow-load-times-eCommerce.png'
+								alt='Book Talk'
 							/>
 							<CardContent>
 								<Typography gutterBottom variant='h5' component='div'>
-									Spaceman Game
+									Book Talk
 								</Typography>
 								<Typography variant='body2'>
-									Spaceman is a browser-based two-player game for word-guessing
-									fanatics! Users are able to input a word to be guessed by
-									their opponent. The game is won when either the word is
-									guessed or you run out of chances. Happy guessing!
+									Have you ever been deep into a good book, and just wanted to
+									converse with someone else who’s equally as enthralled. Or
+									just leave your thoughts in the air. Introduce Book Talk, an
+									application built to host book discussions. Users can login,
+									add a book that they would like to talk about, and leave
+									comments for other readers to respond to.
 								</Typography>
 							</CardContent>
 							<CardActions>
 								<Button
 									size='small'
-									href='https://kgittens89.github.io/spaceman-Game/'
+									href='https://book-talk.netlify.app/'
 									target='_blank'
 									rel='noreferrer'>
 									Deployment
 								</Button>
 								<Button
 									size='small'
-									href='https://github.com/kgittens89/spaceman-Game/tree/main'
+									href='https://github.com/K-Rcode/book-talk-frontend'
 									target='_blank'
 									rel='noreferrer'>
-									Repository
-								</Button>
-							</CardActions>
-						</CardActionArea>
-					</Card>
-
-					<Card
-						sx={{
-							maxWidth: 345,
-							padding: '1em',
-							margin: '1em',
-						}}>
-						<CardActionArea>
-							<CardMedia
-								component='img'
-								height='200'
-								image={cook}
-								alt="Let's Cook"
-							/>
-							<CardContent>
-								<Typography gutterBottom variant='h5' component='div'>
-									Let's Cook
-								</Typography>
-								<Typography variant='body2'>
-									Let's Cook is an application built to accomodate indecisive
-									eaters. You are initially presented with random recipes to
-									choose from. Users can also search for something that may
-									interest them, or generate another random recipe. Happy
-									cooking!
-								</Typography>
-							</CardContent>
-							<CardActions>
-								<Button
-									size='small'
-									href='https://lets-cook-guys.netlify.app/'
-									target='_blank'
-									rel='noreferrer'>
-									Deployment
+									Frontend Repo
 								</Button>
 								<Button
 									size='small'
-									href='https://github.com/kgittens89/lets-cook'
+									href='https://github.com/K-Rcode/book-talk-backend'
 									target='_blank'
 									rel='noreferrer'>
-									Repository
+									Backend Repo
 								</Button>
 							</CardActions>
 						</CardActionArea>
@@ -151,7 +116,8 @@ function Projects() {
 							</CardActions>
 						</CardActionArea>
 					</Card>
-					{/* <Card
+
+					<Card
 						sx={{
 							maxWidth: 345,
 							padding: '1em',
@@ -161,36 +127,82 @@ function Projects() {
 							<CardMedia
 								component='img'
 								height='200'
-								image={flow}
-								alt='The Flow Factory'
+								image={cook}
+								alt="Let's Cook"
 							/>
 							<CardContent>
 								<Typography gutterBottom variant='h5' component='div'>
-									The Flow Factory
+									Let's Cook
 								</Typography>
 								<Typography variant='body2'>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Necessitatibus, optio. Eos quod reiciendis autem corporis?
+									Let's Cook is an application built to accomodate indecisive
+									eaters. You are initially presented with random recipes to
+									choose from. Users can also search for something that may
+									interest them, or generate another random recipe. Happy
+									cooking!
 								</Typography>
 							</CardContent>
 							<CardActions>
 								<Button
 									size='small'
-									href='https://clever-montalcini-9e14ea.netlify.app/'
+									href='https://lets-cook-guys.netlify.app/'
 									target='_blank'
 									rel='noreferrer'>
 									Deployment
 								</Button>
 								<Button
 									size='small'
-									href='https://github.com/kgittens89/yoga-frontend'
+									href='https://github.com/kgittens89/lets-cook'
 									target='_blank'
 									rel='noreferrer'>
 									Repository
 								</Button>
 							</CardActions>
 						</CardActionArea>
-					</Card> */}
+					</Card>
+
+					<Card
+						sx={{
+							maxWidth: 345,
+							padding: '1em',
+							margin: '1em',
+						}}>
+						<CardActionArea>
+							<CardMedia
+								component='img'
+								height='200'
+								image={spaceman}
+								alt='Spaceman Game'
+							/>
+							<CardContent>
+								<Typography gutterBottom variant='h5' component='div'>
+									Spaceman Game
+								</Typography>
+								<Typography variant='body2'>
+									Spaceman is a browser-based two-player game for word-guessing
+									fanatics! Users are able to input a word to be guessed by
+									their opponent. The game is won when either the word is
+									guessed or you run out of chances. Happy guessing!
+								</Typography>
+							</CardContent>
+							<CardActions>
+								<Button
+									size='small'
+									href='https://kgittens89.github.io/spaceman-Game/'
+									target='_blank'
+									rel='noreferrer'>
+									Deployment
+								</Button>
+								<Button
+									size='small'
+									href='https://github.com/kgittens89/spaceman-Game/tree/main'
+									target='_blank'
+									rel='noreferrer'>
+									Repository
+								</Button>
+							</CardActions>
+						</CardActionArea>
+					</Card>
 				</div>
 			</div>
 		</>
