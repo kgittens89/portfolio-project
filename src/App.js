b96051bff22cc588a284568695d9222f21cfb276
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2022-02-25 13:40:10 -0600

    Add fixed navbar for smooth-scroll usability

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Home from './components/Home/Home';
@@ -15,11 +14,6 @@ function App() {
 				<Home/>
 				<AboutMe/>
 				<Projects/>
-				{/* <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<AboutMe />} />
-          <Route path='/projects' element={<Projects />} />
-        </Routes> */}
 			</main>
 			<Footer />
 		</div>
