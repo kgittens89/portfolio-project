d2b37481f5ff6953c28fc0802c2fc63b52bbaa0e
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2022-05-21 15:49:01 -0500

    Add images carousel

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,9 +1,18 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
-import Image from 'react-bootstrap/Image';
 import './AboutMe.css';
 import womanCoding from '../../assets/modern-young-woman-sitting-on-260nw-1928965898.jpeg';
+import keisha from '../../assets/Keisha.jpeg'
+import haddrick from '../../assets/Haddrick.jpeg'
+import louvre from '../../assets/Louvre.JPG'
+import sorting from '../../assets/SortingHat.jpeg'
+import travel from '../../assets/traveling-black-woman.jpeg'
+import meditate from '../../assets/meditate-black-woman.jpeg'
+import dancing from '../../assets/dancing-black-woman.png'
+import roscoe from '../../assets/Roscoe.jpeg'
 
 import { Tooltip } from '@mui/material';
+// import Carousel from 'react-material-ui-carousel';
 import { IoLogoJavascript, IoLogoHtml5, IoLogoCss3 } from 'react-icons/io';
 import { FaReact, FaPython, FaBootstrap } from 'react-icons/fa';
 import {
@@ -13,12 +22,33 @@ import {
 	SiTypescript,
 } from 'react-icons/si';
 
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
 function AboutMe() {
+	const handleDragStart = (e) => e.preventDefault();
+
+	const items = [
+		<img src={womanCoding} onDragStart={handleDragStart} role='presentation' />,
+		<img src={keisha} onDragStart={handleDragStart} role='presentation' />,
+		<img src={travel} onDragStart={handleDragStart} role='presentation' />,
+		<img src={louvre} onDragStart={handleDragStart} role='presentation' />,
+		<img src={dancing} onDragStart={handleDragStart} role='presentation' />,
+		<img src={haddrick} onDragStart={handleDragStart} role='presentation' />,
+		<img src={sorting} onDragStart={handleDragStart} role='presentation' />,
+		<img src={meditate} onDragStart={handleDragStart} role='presentation' />,
+		<img src={roscoe} onDragStart={handleDragStart} role='presentation' style={{objectPosition: 'top'}} />,
+	];
+
+	const Gallery = () => {
+		return <AliceCarousel mouseTracking items={items} />;
+	};
+
     return (
 			<div className='component about'>
 				<h2>About Me</h2>
 				<h4>Software Engineer</h4>
-				<Image src={womanCoding} />
+			{Gallery()}
 				<p>
 					Hi there 👋🏾, I'm Keisha, a Software Engineer, with a background in
 					healthcare, based out of New Orleans, LA. I've recently completed an
